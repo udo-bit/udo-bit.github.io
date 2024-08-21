0cb79d8c2faaaa1ffa8832eab48859678d39cd9c
udo-bit <enote_gxl@163.com>
2024-08-21 13:04:37 +0800

    theme

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -15,7 +15,7 @@ export default hopeTheme({
 
     // iconAssets: "fontawesome",
     // iconPrefix: "fa-brands fa-",
-    iconAssets: '//at.alicdn.com/t/c/font_4647410_vx7rmszfnqd.css',
+    iconAssets: '//at.alicdn.com/t/c/font_4647410_cd5kjrrbc7k.css',
     // iconPrefix: "iconfont icon-",
 
 
