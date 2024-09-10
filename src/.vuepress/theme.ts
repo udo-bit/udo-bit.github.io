b07390b24d22f47025b149ac50ef9babf95e125f
udo-bit <enote_gxl@163.com>
2024-09-10 20:43:14 +0800

    update

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -107,7 +107,7 @@ export default hopeTheme({
         components: {
             components: ["Badge", "VPCard", 'PDF'],
         },
-        
+
 
         // These features are enabled for demo, only preserve features you need here
         mdEnhance: {
