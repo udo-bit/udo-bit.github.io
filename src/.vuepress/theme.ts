4baea30768826d6d21df2040bdf4422cd393b765
udo-bit <enote_gxl@163.com>
2024-10-06 13:44:48 +0800

    CMake笔记.md

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -17,7 +17,7 @@ export default hopeTheme({
 
     // iconAssets: "fontawesome",
     // iconPrefix: "fa-brands fa-",
-    iconAssets: '//at.alicdn.com/t/c/font_4647410_v8zi0jnf9q.css',
+    iconAssets: '//at.alicdn.com/t/c/font_4647410_j21q3xb0b8.css',
     // iconPrefix: "iconfont icon-",
 
 
